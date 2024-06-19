2822aa4da1e3b62507f37667d44423eb1968f3bb
Abraham Navarro <abrahancho6@gmail.com>
2024-06-19 19:58:07 -0400

    feat: adding api key

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware(['with_service_api_key'])->group(function () {
+    Route::apiResource('orders', OrderController::class)->only([
+        'index',
+        'store'
+    ]);
 
-Route::apiResource('orders', OrderController::class)->only([
-    'index',
-    'store'
-]);
-
-Route::apiResource('recipes', RecipeController::class)->only(['index']);
+    Route::apiResource('recipes', RecipeController::class)->only(['index']);
+});
